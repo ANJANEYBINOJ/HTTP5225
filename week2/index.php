ae5f4e7be9a03f9aac22441a4494da84d1401317
Anjaney Binoj <anjaneybinoj20@gmail.com>
2025-05-14 16:38:50 -0400

    add php learning file

---
+++ week2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week2</title>
+</head>
+<body>
+<!-- Learning Echo     -->
+    <?php
+    
+    echo "<h1>Hello World!</h1>";
+
+    echo "<p>My name is Anjaney Binoj</p>"
+    
+    ?>
+
+    <hr>
+
+    <!-- Learning variables -->
+
+    <?php
+
+
+    $fname = "Anjaney";
+    $lname = "Binoj";
+
+    echo "<h1>Hello $fname $lname</h1>"
+
+
+    ?>
+</body>
+</html>
